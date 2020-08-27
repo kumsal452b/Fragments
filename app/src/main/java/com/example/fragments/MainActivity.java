72f5dc9ed6ee0ee7a1ee3208e051bd1766c3caba
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-27 21:28:27 +0300

    Contunie

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +9,17 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private SectionStatePagerAdapter adapter;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"Main started");
+        adapter=new SectionStatePagerAdapter(getSupportFragmentManager());
+        viewPager=findViewById(R.id.containter);
+
+
     }
+    private void setupPager)
 }
--- app/src/main/java/com/example/fragments/fragment1.java
+++ app/src/main/java/com/example/fragments/fragment1.java
@@ -1,8 +1,18 @@
 package com.example.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.zip.Inflater;
 
 public class fragment1 extends Fragment {
     private static final String TAG = "fragment1";
@@ -10,6 +20,41 @@ public class fragment1 extends Fragment {
     private Button btnfrag2;
     private Button btnfrag3;
     private Button btnSeccondActivity;
-    
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment1,container,false);
+        btnfrag1=view.findViewById(R.id.btnfrg1);
+        btnfrag2=view.findViewById(R.id.btnfrg2);
+        btnfrag3=view.findViewById(R.id.btnfrg3);
+        btnSeccondActivity=view.findViewById(R.id.btnSeccondActivity);
+        btnfrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Go to fragment 1",Toast.LENGTH_SHORT);
+            }
+        });
+        btnfrag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Go to fragment 2",Toast.LENGTH_SHORT);
+            }
+        });
+        btnfrag3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Go to fragment 3",Toast.LENGTH_SHORT);
+            }
+        });
+        btnSeccondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Go to fragment Seccond",Toast.LENGTH_SHORT);
+                Intent intent=new Intent(getActivity(),SeccondActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
 }
